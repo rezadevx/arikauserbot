2d0878f052c925fbb66c63b4f8a6a29c2fe4b7c7
rezadevs <sawe57192@gmail.com>
2025-07-20 00:28:23 +0700

    Update prefixes.py

--- plugins/prefixes.py
+++ plugins/prefixes.py
@@ -6,11 +6,14 @@ prefix_db = PrefixDB()
 @Client.on_message(filters.command("setprefix", prefixes=".") & filters.me)
 async def set_prefix(client, message):
     if len(message.command) < 2:
-        return await message.reply("❌ Contoh: <code>.setprefix !</code>")
+        return await message.reply(
+            "❌ Format salah.\nContoh: <code>.setprefix !</code>"
+        )
 
     new_prefix = message.command[1]
+
     if len(new_prefix) > 2:
         return await message.reply("❌ Prefix terlalu panjang. Maksimal 2 karakter.")
 
     await prefix_db.set_prefix(message.from_user.id, new_prefix)
-    await message.reply(f"✅ Prefix berhasil diubah menjadi: <code>{new_prefix}</code>")
+    await message.reply(f"✅ Prefix kamu berhasil diubah ke: <code>{new_prefix}</code>")
